ba97388c5782a36166e15d38ac5436cee3d50611
taaaaho <taaaaho@gmail.com>
2023-04-10 20:55:30 +0900

    feat: push and receive

--- src/pages/api/push.ts
+++ src/pages/api/push.ts
@@ -1,24 +1,38 @@
-import * as PushAPI from "@pushprotocol/restapi";
-import * as ethers from "ethers";
+import * as PushAPI from '@pushprotocol/restapi'
+import * as ethers from 'ethers'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const { title, message } = req.query
+  switch (req.method) {
+    case 'POST': {
+      const Pkey = process.env.PRIVATE_KEY || ''
+      const _signer = new ethers.Wallet(Pkey)
+      sendNotification(title as string, message as string, _signer)
+      return res.status(200).json({ status: 'Success' })
+    }
+    case 'GET': {
+      const notifications = await PushAPI.user.getFeeds({
+        user: 'eip155:5:0x785a58b8A172e98756b8CeCaD674ab35da4e380e', // user address in CAIP
+        raw: true,
+        // @ts-ignore
+        env: 'staging',
+      })
+      const parsedResults = PushAPI.utils.parseApiResponse(notifications)
+      console.log(parsedResults)
+      return res.status(200).json({ status: 'Success', message: parsedResults })
+    }
+  }
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-
-const Pkey = process.env.PRIVATE_KEY || '';
-const _signer = new ethers.Wallet(Pkey);
-
-console.log(req.query)
-const {title, message} = req.query
-
-
-const sendNotification = async() => {
+const sendNotification = async (
+  title: string,
+  message: string,
+  _signer: ethers.Signer
+) => {
   try {
     const apiResponse = await PushAPI.payloads.sendNotification({
       signer: _signer,
@@ -26,23 +40,19 @@ const sendNotification = async() => {
       identityType: 2,
       notification: {
         title: `[SDK-TEST] notification TITLE:`,
-        body: `[sdk-test] notification BODY`
+        body: `[sdk-test] notification BODY`,
       },
       payload: {
         title: title as string,
         body: message as string,
         cta: '',
-        img: ''
+        img: '',
       },
       channel: 'eip155:5:0x785a58b8A172e98756b8CeCaD674ab35da4e380e',
-        // @ts-ignore
-      env: 'staging'
-    });
+      // @ts-ignore
+      env: 'staging',
+    })
   } catch (err) {
-    console.error('Error: ', err);
+    console.error('Error: ', err)
   }
-}
-
-sendNotification();
-  res.status(200).json({ name: 'John Doe' })
 }
