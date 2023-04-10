ebeb74a64e51b06d1bb14799a6a7abed8f442e9d
taaaaho <taaaaho@gmail.com>
2023-04-10 21:50:55 +0900

    chore: thirdweb

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebProvider activeChain={ChainId.Goerli}>
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  )
 }
